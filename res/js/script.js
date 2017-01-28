cb60e8a8e1bae826a0393d517bee8e090015ca5e
Damian Heaton <dh64784@gmail.com>
2017-01-28 23:48:13 +0000

    Started work on modern design (speed-centric)

---
+++ res/js/script.js
@@ -0,0 +1,40 @@
+var dk = false;
+
+function start() {
+  try {
+    $(window).resize(() => {
+      if($(window).width() >= 1000) {
+        dk = true;
+        $(document.body).addClass("dk");
+      }else{
+        dk = false;
+        $(document.body).removeClass("dk");
+      }
+    });
+
+    $(window).resize();
+
+    $(document).ready(() => {
+      $('html').mousemove((e) => {
+        if(dk){
+          var h1 = document.getElementsByTagName("h1")[0];
+          h1.style.left = ($(window).width() / 2) + (e.pageX - ($(window).width() / 2)) / 50 + "px";
+          h1.style.top = ($(window).height() / 2 - 103.5) + (e.pageY - ($(window).height() / 2)) / 50 + "px";
+        }
+      });
+    });
+  }catch(e){
+    setTimeout(start, 1);
+  }
+}
+
+start();
+
+function showModal(id){
+  var div = document.getElementById(id);
+  div.style.display = "block";
+}
+function hideModal(id){
+  var div = document.getElementById(id);
+  div.style.display = "none";
+}
